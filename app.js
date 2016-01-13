4a552fce3bf6ed7d12044013f80a6b9cc9ca5129
dahonen <daveahonen@yahoo.com>
2016-01-13 12:11:24 -0700

    Added a new greeting prompt.

--- app.js
+++ app.js
@@ -2,3 +2,7 @@ console.log("DevMountain is awesome!!!");
 console.log("Learning how to commit to git hub");
 console.log("Making a change to the js file");
 console.log("Greetings and salutations");
+var myName = prompt("Dave Ahonen");
+if (myName = "Dave Ahonen") {
+}
+  alert("Nice to meet you Dave Ahonen");
