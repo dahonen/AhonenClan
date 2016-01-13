6d283a95b50891483bbc33b5ef5b3f22c76cf521
dahonen <daveahonen@yahoo.com>
2016-01-12 20:47:06 -0700

    New git commit

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
 console.log("DevMountain is awesome!!!");
 console.log("Learning how to commit to git hub");
-console.log("Making a change to the js file")
+console.log("Making a change to the js file");
+console.log("Greetings and salutations");
